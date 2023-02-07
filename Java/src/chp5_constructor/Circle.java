d599481f97d8ec2aa27d82dfb70c2eed40e677cf
pyg410 <pyg410@naver.com>
2023-02-07 17:51:24 +0900

    add: Object dir

---
+++ Java/src/chp5_constructor/Circle.java
@@ -0,0 +1,24 @@
+public class Circle {
+    public static void main(String[] args) {
+        Circle pizza = new Circle(10, "자바피자");
+
+        double area = pizza.getArea();
+        System.out.println(area);
+    }
+        int radius;
+        String name;
+
+        public Circle(){
+            radius=1;
+            name="";
+        }
+
+        public Circle(int r, String n){
+            radius=r;
+            name=n;
+        }
+
+        public double getArea(){
+            return 3.14*radius*radius;
+        }
+    }
