e8416071bbbbfebb3ec707df12d4c2fd88526529
park <pyg410@naver.com>
2023-02-15 20:41:38 +0900

    add: ModernJavaInAction

--- Object/src/TicketSeller.java
+++ Object/src/TicketSeller.java
@@ -5,10 +5,12 @@ import java.util.List;
 public class TicketSeller {
     private TicketOffice ticketOffice;
 
-    public TicketSeller(TicketOffice ticketOffice){
-        this.ticketOffice=ticketOffice;
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
     }
-    public TicketOffice getTicketOffice(){
-        return ticketOffice;
+
+    public void sellTo(Audience audience){
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
+
 }
