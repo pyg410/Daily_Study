62ac722349381ec5ffcbce5f064c90ae7282d5e1
Cookie <74770498+pyg410@users.noreply.github.com>
2023-03-14 02:13:24 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 416 ms, Memory: 38936 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
@@ -1,48 +1,46 @@
 import java.io.*;
-import java.util.*;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
 public class Main {
-    static boolean visited[] = new boolean [1<<21];
-    static int n,m;
-    static int arr[];
-    static int ans =0;
-    public static void main(String[] args) throws IOException {
+    static int[] train;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String [] t = br.readLine().split(" ");
-        n = Integer.parseInt(t[0]);         // 기차의 개수
-        m = Integer.parseInt(t[1]);     // 명령의 개수
-        arr = new int[n+1];
-        
-        for(int i=0; i<m; i++) {
-            String[] tt = br.readLine().split(" ");
-            int order = Integer.parseInt(tt[0]);
-            int train = Integer.parseInt(tt[1]);
-            
-            if(order == 1) {
-                int seat = Integer.parseInt(tt[2]);
-                arr[train] = (arr[train] | (1<<seat));
-            }
-            if(order == 2) {
-                int seat = Integer.parseInt(tt[2]);
-                arr[train] = arr[train]  & ~(1<<seat);
-            }
-            if(order == 3) {
-                arr[train] =  arr[train] <<1;
-                arr[train] = arr[train] & ((1<<21)-1);
-            }
-            if(order ==4) {
-                arr[train] = arr[train] >> 1;
-                arr[train] = arr[train]& ~1;
-            }
-        }
-        
-        for(int i=1; i<=n; i++) {
-            if(!visited[arr[i]]) {
-                ans++;
-                visited[arr[i]] = true;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        train = new int[N+1];
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int cmd = Integer.parseInt(st.nextToken());
+            int trainIdx = Integer.parseInt(st.nextToken());
+            int seatNum = st.hasMoreTokens()?(Integer.parseInt(st.nextToken())):0;
+            switch(cmd){
+                case 1:{
+                    train[trainIdx] = train[trainIdx]|(1<<seatNum);
+                    break;
+                }
+                case 2:{
+                    train[trainIdx] = train[trainIdx] & ~(1<<seatNum);
+                    break;
+                }
+                case 3:{
+                    train[trainIdx] = train[trainIdx]<<1;
+                    train[trainIdx] = (train[trainIdx] & ~(1<<21));
+                    break;
+                }
+                case 4:{
+                    train[trainIdx] = train[trainIdx]>>1;
+                    train[trainIdx] = (train[trainIdx] & (~1));
+                    break;
+                }
             }
         }
-        
-        System.out.println(ans);
+        HashSet<Integer> trainValidate = new HashSet<>();
+        for(int i=1; i<N+1; i++){
+            trainValidate.add(train[i]);
+        }
+        System.out.println(trainValidate.size());
     }
 }
